33195518f96fa3688a6f1d4391612ad15ed4825b
Andre <lulesaaso2@gmail.com>
2023-06-24 15:28:54 -0500

    change URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/AnLeo002/React-TaskList'
+  base:'https://anleo002.github.io/React-TaskList/'
 })
